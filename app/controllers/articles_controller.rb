6c8cfe1550c77294de6698cc01b6e4817ed37bc5
= <andrespechtbs@googlemail.com>
2018-06-09 19:27:01 +0200

    new and show action index and stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
     
+    def index
+        @articles = Article.all
+    end
+
     def new
         @article = Article.new
     end
@@ -16,6 +20,10 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def article_params
         params.require(:article).permit(:title, :description)
     end
@@ -23,5 +31,15 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
         
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was succesfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
     end
 end
